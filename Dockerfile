5156d425ffe5b21feef60bf6f63427eb4840ba7b
Takuya TAKAHASHI <takahashi@bank.co.jp>
2018-08-02 15:24:46 +0900

    mod docker image

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,5 @@ RUN go build
 FROM google/cloud-sdk:slim
 COPY --from=builder /go/src/github.com/takutakahashi/k8s-docker-image-builder/k8s-docker-image-builder /kdib
 ADD start.sh /start.sh
+ADD ssh_config /root/.ssh/config
 CMD '/start.sh'
--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -e
 ls /secrets/credentials.json
 ls /root/.ssh/id_rsa
+ls /var/run/docker.sock
 gcloud auth activate-service-account --key-file=/secrets/credentials.json
 gcloud --quiet auth configure-docker
-ssh git@github.com
+ssh git@github.com || echo "known_hosts created"
 /kdib
