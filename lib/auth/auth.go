24da7840978821b71788c73c4234dc150fac3ac6
Takuya TAKAHASHI <takahashi@bank.co.jp>
2018-08-06 01:07:06 +0900

    add auth method

---
+++ lib/auth/auth.go
@@ -0,0 +1,5 @@
+package auth
+
+func Check(requestToken string) bool {
+	return true
+}
--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,9 @@
 package routes
 
 import (
+	"fmt"
 	"github.com/labstack/echo"
+	"github.com/takutakahashi/k8s-docker-image-builder/lib/auth"
 	"github.com/takutakahashi/k8s-docker-image-builder/lib/builder"
 	"net/http"
 )
@@ -12,18 +14,27 @@ func check(err error) {
 	}
 }
 
-func setResponseBase(c echo.Context) {
+func setResponseBase(c echo.Context) error {
+	if !auth.Check(c.FormValue("token")) {
+		c.Response().WriteHeader(http.StatusForbidden)
+		return fmt.Errorf("not authorized token")
+	}
 	c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	c.Response().WriteHeader(http.StatusOK)
+	return nil
 }
 
 func build(c echo.Context) error {
+	err := setResponseBase(c)
+	check(err)
 	image := c.FormValue("image")
 	builder.Build(c, builder.GetTarFile(c), image)
 	return nil
 }
 
 func publish(c echo.Context) error {
+	err := setResponseBase(c)
+	check(err)
 	image, repo, branch := c.FormValue("image"), c.FormValue("repo"), c.FormValue("branch")
 	builder.BuildFromRepo(c, repo, branch, image)
 	builder.Push(c, image)
@@ -31,14 +42,16 @@ func publish(c echo.Context) error {
 }
 
 func pull(c echo.Context) error {
-	setResponseBase(c)
+	err := setResponseBase(c)
+	check(err)
 	image := c.FormValue("image")
 	builder.Pull(c, image)
 	return nil
 }
 
 func push(c echo.Context) error {
-	setResponseBase(c)
+	err := setResponseBase(c)
+	check(err)
 	image := c.FormValue("image")
 	builder.Push(c, image)
 	return nil
